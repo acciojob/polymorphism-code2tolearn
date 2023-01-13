f174b1e16a81c04e981100b5850b729f1cecc8b8
Shailesh <gajbhiye719@gmail.com>
2023-01-13 10:33:22 +0530

    solution 3 Polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,9 +9,14 @@ public class Main {
         Scanner sc = new Scanner(System.in) ;
         int x = sc.nextInt() ;
         int y = sc.nextInt() ;
+        int z = sc.nextInt() ;
         Product p = new Product() ;
         int result =   p.product(x,y) ;
         System.out.println(result);
+        System.out.println(p.product(x,y,z));
+        double result1 = p.product((double)x,(double)y) ;
+        System.out.println(result1);
+
     }
 
 }
--- src/main/java/com/driver/Product.java
+++ src/main/java/com/driver/Product.java
@@ -5,4 +5,12 @@ public class Product {
     {
         return x*y ;
     }
+    public int product(int x, int y, int z)
+    {
+        return x*y*z ;
+    }
+    public double product(double x, double y) {
+        return (double) x*y ;
+    }
+
 }
