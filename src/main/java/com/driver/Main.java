1e7998cb4423a76ce3520001ceaac32144bf1a22
Shailesh <gajbhiye719@gmail.com>
2023-01-13 10:17:08 +0530

    Solution 2 Polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,14 +4,6 @@ import java.util.Scanner;
 
 public class Main {
 
-    static class Product {
-
-        public int product(int x, int y)
-        {
-            return x*y ;
-        }
-    }
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in) ;
---
+++ src/main/java/com/driver/Product.java
@@ -0,0 +1,8 @@
+package com.driver;
+
+public class Product {
+    public int product(int x, int y)
+    {
+        return x*y ;
+    }
+}
